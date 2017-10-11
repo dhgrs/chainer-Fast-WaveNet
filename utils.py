264ba4aea100dbd0c809c2e9d972a1f91ef38ed8
dhgrs <d.higurashi@gmail.com>
2017-10-12 01:41:46 +0900

    update utilities

--- utils.py
+++ utils.py
@@ -1,12 +1,14 @@
 # coding: UTF-8
 import random
+import os
 
 import numpy as np
 import librosa
+from matplotlib import pyplot as plt
 
 
 class mu_law(object):
-    def __init__(self, mu=255, int_type=np.int32, float_type=np.float32):
+    def __init__(self, mu=256, int_type=np.uint8, float_type=np.float32):
         self.mu = mu
         self.int_type = int_type
         self.float_type = float_type
@@ -14,14 +16,13 @@ class mu_law(object):
     def transform(self, x):
         x = x.astype(self.float_type)
         y = np.sign(x) * np.log(1 + self.mu * np.abs(x)) / np.log(1 + self.mu)
-        y = np.digitize(y, 2 * np.arange(1 + self.mu) / self.mu - 1) - 1
-        y = np.identity(self.mu + 1)[y.astype(self.int_type)]
+        y = np.digitize(y, 2 * np.arange(self.mu) / self.mu - 1) - 1
         return y.astype(self.int_type)
 
     def itransform(self, y):
         y = y.astype(self.float_type)
         y = 2 * y / self.mu - 1
-        x = np.sign(y) / self.mu * ((1 + self.mu) ** np.abs(y) - 1)
+        x = np.sign(y) / self.mu * ((self.mu) ** np.abs(y) - 1)
         return x.astype(self.float_type)
 
 
@@ -34,15 +35,28 @@ class Preprocess(object):
         self.random = random
 
     def __call__(self, path):
-        x = self.read_file(path)
-        if self.random:
-            start = random.randint(0, len(x) - self.length-1)
-            x = x[start:start + self.length]
+        # load data
+        npy_path = path.replace('.flac',
+                                '_{}_{}.npy'.format(self.sr, self.mu+1))
+        if os.path.exists(npy_path):
+            t = np.load(npy_path)
         else:
-            x = x[:self.length]
-        y = self.mu_law.transform(x)
-        y = np.expand_dims(y.T, 1)
-        return y[:, :, :-1].astype(mu_law.float_type), y[:, :, 1:]
+            x = self.read_file(path)
+            t = self.mu_law.transform(x)
+            np.save(npy_path, t)
+
+        # triming
+        if self.random:
+            start = random.randint(0, len(t) - self.length-1)
+            t = t[start:start + self.length]
+        else:
+            t = t[:self.length]
+
+        # expand dimension
+        y = np.identity(self.mu + 1)[t].astype(np.float32)
+        y = np.expand_dims(y.T, 2)
+        t = np.expand_dims(t.astype(np.int32), 1)
+        return y[:, :-1, :], t[1:, :]
 
     def read_file(self, path):
         x, sr = librosa.core.load(path, self.sr, res_type='kaiser_fast')
